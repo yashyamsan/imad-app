b6c8b9b218fae730ac07fbee8c020e48eb5397d7
yashyamsan <yashyam.yy@gmail.com>
2017-08-19 15:50:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
