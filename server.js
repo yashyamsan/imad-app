916d6cdfdc0eb47f80b6a74afbcbcd5c801fac0b
yashyamsan <yashyam.yy@gmail.com>
2017-08-19 14:45:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
- res.sendFile(path.join(__dirname,'ui','article-one.html'))
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
